22fe6c831932b17e53e4b4f5b5cc25ae0f71bfea
aryan-92 <aryanpradhan91@gmail.com>
2023-03-28 19:18:08 +0530

    commit

--- app/Http/Controllers/MarkEntryController.php
+++ app/Http/Controllers/MarkEntryController.php
@@ -6,15 +6,44 @@ use App\Models\CourseFor;
 use App\Models\StudentDetails;
 use Illuminate\Http\Request;
 use DB;
+use DataTables;
 
 class MarkEntryController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // return $student = DB::table('ug_examination_applications as up_app')->select('up_app.*','sd.*')
-        // ->join('student_details as sd','sd.id','up_app.stu_id')
-        // ->get();
-        $student = DB::table('student_details as sd')->select('ug_app.id as ug_id','ug_app.stu_id as ug_stu_id','ug_app.payment_status as ug_payment_status','ug_app.form_status as ug_form_status','sd.*','pg_app.*','pg_app.id as pg_id','pg_app.stu_id as pg_stu_id','pg_app.payment_status as pg_payment_status','pg_app.form_status as pg_form_status','sd.id as student_id','c.name as course_name','c.id as course_id')
+        
+        // return $student = DB::table('student_details as sd')->select('ug_app.id as ug_id','ug_app.stu_id as ug_stu_id','ug_app.payment_status as ug_payment_status','ug_app.form_status as ug_form_status','sd.*','pg_app.*','pg_app.id as pg_id','pg_app.stu_id as pg_stu_id','pg_app.payment_status as pg_payment_status','pg_app.form_status as pg_form_status','sd.id as student_id','c.name as course_name','c.id as course_id')
+        // ->leftJoin('ug_examination_applications as ug_app','sd.id','ug_app.stu_id')
+        // ->leftJoin('pg_examination_applications as pg_app','sd.id','pg_app.stu_id')
+        // ->leftJoin('courses as c','c.id','sd.course_id')
+        // ->orWhere('ug_app.form_status',2)
+        // ->orWhere(['ug_app.payment_status' => 1])
+        // ->orWhere('pg_app.form_status',2)
+        // ->orWhere('pg_app.payment_status',1)
+        // // ->where('up_app.form_status',2)
+        // ->get(['batch_year']);
+         $student = DB::table('student_details as sd')
+        ->leftJoin('ug_examination_applications as ug_app','sd.id','ug_app.stu_id')
+        ->leftJoin('pg_examination_applications as pg_app','sd.id','pg_app.stu_id')
+        ->leftJoin('courses as c','c.id','sd.course_id')
+        ->orWhere('ug_app.form_status',2)
+        ->orWhere(['ug_app.payment_status' => 1])
+        ->orWhere('pg_app.form_status',2)
+        ->orWhere('pg_app.payment_status',1)
+        // ->where('up_app.form_status',2)
+        ->get('batch_year');
+
+        $department  = CourseFor::get();
+        
+        
+        //dd($collection);
+        $collection = collect($student);
+        $all_batch_year = $collection->unique('batch_year');
+
+        if($request->ajax())
+        {
+         $student2 = DB::table('student_details as sd')->select('ug_app.id as ug_id','ug_app.stu_id as ug_stu_id','ug_app.payment_status as ug_payment_status','ug_app.form_status as ug_form_status','sd.*','pg_app.*','pg_app.id as pg_id','pg_app.stu_id as pg_stu_id','pg_app.payment_status as pg_payment_status','pg_app.form_status as pg_form_status','sd.id as student_id','c.name as course_name','c.id as course_id')
         ->leftJoin('ug_examination_applications as ug_app','sd.id','ug_app.stu_id')
         ->leftJoin('pg_examination_applications as pg_app','sd.id','pg_app.stu_id')
         ->leftJoin('courses as c','c.id','sd.course_id')
@@ -24,9 +53,46 @@ class MarkEntryController extends Controller
         ->orWhere('pg_app.payment_status',1)
         // ->where('up_app.form_status',2)
         ->get();
+        
+        //return $request;
+
+         // dd($student2);
+
+            return DataTables::of($student2)
+            ->addIndexColumn()
+            ->filter(function ($instance) use ($request) {
+                //dd( $request->get('batch_year'));
+                if (!empty($request->get('batch_year'))) {
+                    $instance->collection = $instance->collection->where('batch_year', $request->get('batch_year'));
+                }
+                if (!empty($request->get('sem_id'))) {
+                    $instance->collection = $instance->collection->where('current_semester', $request->get('sem_id'));
+                }
+                if (!empty($request->get('dep_val'))) {
+                    $instance->collection = $instance->collection->where('department_id', $request->get('dep_val'));
+                }
+                
+
+             })
+
+            ->addColumn('name', function ($row) {
+                //dd($row['name']);
+                $name = $row->name;
+                return $name;
+            })
+            ->addColumn('college_name', function ($row) {
+                //dd($row['name']); student_id
+                $college_name =  StudentDetails::find($row->student_id);
+                $college_name =  $college_name->collegeName();
+
+                return $college_name;
+            })
+            ->rawColumns(['name','college_name'])
+            ->make(true);
+        }
       
         
-        $department  = CourseFor::get();
-        return view('mark_entry.index',compact('student','department'));
+        
+        return view('mark_entry.index',compact('student','department','all_batch_year'));
     }
 }
--- resources/views/mark_entry/index.blade.php
+++ resources/views/mark_entry/index.blade.php
@@ -6,7 +6,7 @@
             <div id="panel-1" class="panel">
                 <div class="panel-hdr">
                     <h2>
-                         Mark Entry  
+                        Mark Entry
                     </h2>
                 </div>
                 <div class="panel-container show">
@@ -17,25 +17,25 @@
                                     <label class="form-label">Select year</label>
                                     <select name="" class="form-control" id="batch_year">
                                         <option value="">Select Batch Year</option>
-                                        <option value="all">All</option>
-                                        {{-- @foreach ($all_batch_year as $item)
+                                        <option value="">All</option>
+                                        @foreach ($all_batch_year as $item)
                                             <option value="{{ $item->batch_year }}">{{ $item->batch_year }}</option>
-                                        @endforeach --}}
+                                        @endforeach
                                     </select>
                                 </div>
                                 <div class="col-md-4">
                                     <label class="form-label">Select Semester</label>
-                                    <select name="" class="form-control" id="">
+                                    <select name="" class="form-control" id="sem_id">
                                         <option value="">Select Semester</option>
                                         @for ($i = 1; $i <= 8; $i++)
-                                            
-                                                <option value="{{ $i }}">{{ $i }}</option>
-                                            
+                                            <option value="{{ $i }}">{{ $i }}</option>
+
                                             {{-- @if ($i % 2 != 0)
                                                 <option value="{{ $i }}">{{ $i }}</option>
                                             @endif --}}
                                         @endfor
                                     </select>
+                                    <input type="hidden" value="1" id="dep_id">
                                 </div>
                                 <div class="col-md-4">
                                     {{-- <input type="button" class="btn btn-sm btn-success waves-effect waves-themed" value="search"> --}}
@@ -44,52 +44,40 @@
                             </div>
 
                         </div>
-                        
+
                         <ul class="nav nav-tabs nav-tabs-clean" role="tablist">
                             @foreach ($department as $key => $item)
-                            <li class="nav-item"><a class="nav-link {{$key == 0 ? 'active' : ''}}" data-toggle="tab" href="#tab-{{$item->id}}" role="tab">{{$item->course_for}}</a></li>
+                                <li class="nav-item"><a class="nav-link das {{ $key == 0 ? 'active' : '' }}" data-toggle="tab"
+                                        href="#tab-{{ $item->id }}" role="tab" dep-id="{{ $item->id }}" >{{ $item->course_for }}</a></li>
                             @endforeach
-                            
+
                             {{-- <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#tab-profile" role="tab">pg</a></li> --}}
                         </ul>
                         <div class="tab-content p-3">
                             @foreach ($department as $key => $value)
-                            <div class="tab-pane fade {{$key == 0 ? 'show active' : ''}}" id="tab-{{$value->id}}" role="tabpanel" aria-labelledby="tab-{{$value->id}}">
-                                <table class="table table-bordered table-hover table-striped w-100 dataTable12">
-                                    <thead>
-                                        <tr>
-                                            
-                                            <th>Name</th>
-                                            <th>College Name</th>
-                                            {{-- <th>Department</th> --}}
-                                            <th>Course</th>
-                                            <th>Semester</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($student as $item)
-                                        
-                                        @if ($item->department_id == $value->id)
-                                        <tr>
-                                            <td>{{ $item->name }}</td>
-                                            <td>{{ $item->clg_id }}</td>
-                                            {{-- <td>{{ $item->department->course_for }}</td> --}}
-                                            <td>{{ $item->course_name }}</td>
-                                            <td>{{$item->current_semester}}</td>
-                                            <td>Action</td>
-                                        </tr>
-                                        @endif
-                                       
-                                        @endforeach
-                                        
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
+                                <div class="tab-pane fade {{ $key == 0 ? 'show active' : '' }}" id="tab-{{ $value->id }}"
+                                    role="tabpanel" aria-labelledby="tab-{{ $value->id }}">
+                                    <table
+                                        class="table table-bordered table-hover table-striped w-100 dataTable1">
+                                        <thead>
+                                            <tr>
+                                                <th>Sl.No</th>
+                                                <th>Name</th>
+                                                <th>College Name</th>
+                                                
+                                                <th>Course</th>
+                                                <th>Semester</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </div>
                             @endforeach
-                            <div class="tab-pane fade" id="tab-profile" role="tabpanel" aria-labelledby="tab-profile"></div>
-                            
+                            {{-- <div class="tab-pane fade" id="tab-profile" role="tabpanel" aria-labelledby="tab-profile"></div> --}}
+
                         </div>
 
                     </div>
@@ -99,109 +87,87 @@
 
         </div>
     </div>
-
-
-    
 @endsection
 
 @section('js')
-<script>
-    $(document).ready(function(){
+    <script>
+        // $(document).ready(function() {
+        //     $('.dataTable12').DataTable();
+        // });
         
-        // add Examined Section
-        var countOtherDocRow = 1;
-        var counterOtherDocRow = 0;
-        
-            $('#button_examine').click(function(e) {
-                alert('hi');
-                e.preventDefault();
-                var bde_course = $('#bde_course').val();
-                var bde_theo_prac = $('#bde_theo_prac').val();
-                var bde_description = $('#bde_description').val();
 
-                
-                    var newRow = '<tr>' +
-                        '<td>' + bde_course +
-                        '<input type="hidden" name="bde_course_hid[]" value="' +
-                        bde_course + '"></td>' +
+        $(function() {
+            //alert($dep_id);
 
-                        '<td>' + bde_theo_prac +
-                        '<input type="hidden" name="bde_theo_prac_hid[]" value="' +
-                        bde_theo_prac + '"></td>' +
+            var table = $('.dataTable1').DataTable({
+                processing: true,
+                serverSide: true,
+                orderable: false,
+                searching: false,
+                ajax: {
+                    //url: "{{ route('student_list_ajax') }}",
+                    url: "{{ route('mark_list') }}",
+                    type: 'GET',
+                    data: function(d) {
+                        d.batch_year = $('#batch_year').val(),
+                        d.sem_id = $('#sem_id').val(),
+                        d.dep_val = $('#dep_id').val()
+                        // d.stu_name = $('#stu_name').val()
+                        // d.search = $('input[type="search"]').val()
+                    },
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'college_name',
+                        name: 'college_name'
+                    },
+                    {
+                        data: 'course_name',
+                        name: 'course_name'
+                    },
+                    {
+                        data: 'current_semester',
+                        name: 'current_semester'
+                    },
 
-                        '<td>' + bde_description +
-                        '<input type="hidden" name="bde_description_hid[]" value="' +
-                        bde_description + '"></td>' +
+                    {
+                        data: 'action',
+                        name: 'action',
+                        render: function(data, type, row) {
 
-                        
-                        '<td><button type="button" class="btn btn-outline-warning waves-effect waves-themed btn-sm remove_field" id="' +
-                        counterOtherDocRow + '">Remove</button></td></tr>';
-                    $('#addExamined').append(newRow);
-                    countOtherDocRow++;
-                    counterOtherDocRow++;
-                
+                            return 'action';
+                        }
+                    },
+                ]
             });
 
-            $("#addExamined").on("click", ".remove_field", function(e) {
-                // alert('1');
-                $(this).parent('td').parent('tr').remove();
-                counterOtherDocRow--;
-                countOtherDocRow--;
+            $('#batch_year').change(function() {
+                // alert('hi');
+                table.draw();
             });
-
-
-            // End Examined Section
-
-
-            var countBDE = 1;
-            var counterBDERow = 0;
-        
-            $('#button_BDE').click(function(e) {
-                alert('hi');
-                e.preventDefault();
-                var bde_year = $('#bde_year').val();
-                var bde_exam = $('#bde_exam').val();
-                var bde_roll_no = $('#bde_roll_no').val();
-                var bde_regd_no = $('#bde_regd_no').val();
-
-                
-                    var newRow = '<tr>' +
-                        '<td>' + bde_year +
-                        '<input type="hidden" name="bde_year_hid[]" value="' +
-                        bde_year + '"></td>' +
-
-                        '<td>' + bde_exam +
-                        '<input type="hidden" name="bde_exam_hid[]" value="' +
-                        bde_exam + '"></td>' +
-
-                        '<td>' + bde_roll_no +
-                        '<input type="hidden" name="bde_roll_no_hid[]" value="' +
-                        bde_roll_no + '"></td>' +
-
-                        '<td>' + bde_regd_no +
-                        '<input type="hidden" name="bde_regd_no_hid[]" value="' +
-                        bde_regd_no + '"></td>' +
-
-                        
-                        '<td><button type="button" class="btn btn-outline-warning waves-effect waves-themed btn-sm remove_field" id="' +
-                            counterBDERow + '">Remove</button></td></tr>';
-                    $('#addBDE').append(newRow);
-                    countBDE++;
-                    counterBDERow++;
-                
+            $('#sem_id').change(function() {
+                // alert('hi');
+                table.draw();
             });
-
-            $("#addBDE").on("click", ".remove_field", function(e) {
-                // alert('1');
-                $(this).parent('td').parent('tr').remove();
-                countBDE--;
-                counterBDERow--;
+            $(".das").on("click", function() {
+                 $('#dep_id').val($(this).attr('dep-id'));
+                 table.draw();
             });
+            // $("#stu_name").focusout(function() {
+            //     table.draw();
+            // });
+            // $("#stu_name").keyup(function() {
+            //     table.draw();
+            // });
 
-
-
-
-        
-    });
-</script>
+        });
+    </script>
 @endsection
